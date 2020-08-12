41a6abc1c84570ab1bf2769b46a573510c6f222f
b <brock.elmore@gmail.com>
2020-08-12 17:38:23 -0400

    save yam

---
+++ migrations/2_deploy_savior.js
@@ -0,0 +1,39 @@
+
+
+// ============ Contracts ============
+
+// Governance
+// deployed third
+const SaviorGov = artifacts.require("GovernorAlpha");
+const Timelock = artifacts.require("Timelock");
+
+
+
+// ============ Main Migration ============
+
+const migration = async (deployer, network, accounts) => {
+  await Promise.all([
+    deploySaveGovernance(deployer, network),
+  ]);
+};
+
+module.exports = migration;
+
+
+async function deployDistribution(deployer, network) {
+  let curr_gov = "0x6abA376e3331E3090456495e8292ecdFA1ab4920"
+  await deployer.deploy(SaviorGov);
+  let gov = new web3.eth.Contract(Gov.abi, curr_gov);
+  gov.methods.propose(
+      [
+        "0xaE99fF8fe2236AF5083Ea979Ecf1DbAA0EFD07E3", "0x0e2298E3B3390e3b945a5456fBf59eCc3f55DA16"
+      ],
+      [0, 0],
+      ["4dd18bf5", "fa8f3455"], // bytes4(keccak256(setPendingAdmin(address))), bytes4(keccak256(_setRebaser(address))),
+      [
+          SaviorGov.address,
+          "0x0000000000000000000000000000000000000000"
+      ],
+      "Save YAM. Decrease quorum & threshold to static amounts, Remove rebasing."
+  ).send({from: "0x683A78bA1f6b25E29fbBC9Cd1BFA29A51520De84", gas: 1000000});
+}
